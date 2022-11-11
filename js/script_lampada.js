0c98ce16b9cf32de227404d275c4a81e7db5f95f
Lisiane Machado Martins <lisiannemachado@gmail.com>
2022-11-11 16:48:24 -0300

    lampada finalizada

---
+++ js/script_lampada.js
@@ -0,0 +1,32 @@
+const turnOn = document.getElementById ('turnOn');
+const turnOff = document.getElementById ('turnOff');
+const lamp = document.getElementById ('lamp');
+
+
+function lampOn () {
+	if (!isLampBroken () ){
+	lamp.src = './img/ligada.png';
+	}
+}
+
+function lampOff (){
+	if (!isLampBroken () ){
+	lamp.src = './img/desligada.png';
+	}
+}
+
+function lampBroken () {
+	lamp.src = './img/quebrada.png';
+
+}
+
+function isLampBroken (){
+	return lamp.src.indexOf ('quebrada') > -1;
+
+}
+
+turnOn.addEventListener ('click', lampOn);
+turnOff.addEventListener ('click', lampOff);
+lamp.addEventListener ('mouseover', lampOn);
+lamp.addEventListener ('mouseleave', lampOff);
+lamp.addEventListener ('dblclick', lampBroken);
